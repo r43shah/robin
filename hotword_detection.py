3df0e5697f97d9549d3fc3d8ccdfa7af80f8c193
TranBrian10 <tranbrian10@gmail.com>
2017-05-28 00:29:36 -0400

    Minor change for listening indicator

--- hotword_detection.py
+++ hotword_detection.py
@@ -1,11 +1,13 @@
 import snowboydecoder
 
 def detected_callback():
-    print "hotword detected"
+    print ('hotword detected')
 	# Stop the  detector until google assistant is done
 	# Turn on google assistant
 	
 detector = snowboydecoder.HotwordDetector("Robin.pmdl", sensitivity=0.5, audio_gain=1)
 detector.start(detected_callback)
+print('Listening...')
 
 detector.terminate()
+print('Stopped listening')
