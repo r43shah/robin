a27ebd20499e7d3ba6e447bae8a5bd046e8f0c1a
Aditya Matam <matamaditya@gmail.com>
2017-05-28 20:38:10 -0400

    testing modified hotword

--- hotword_detection.py
+++ hotword_detection.py
@@ -1,7 +1,27 @@
 import snowboydecoder
+from google.assistant import *
 
 def detected_callback():
-    print "hotword detected"
+    print("hotword_detection: hotword detected")
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--credentials', type=existing_file,
+                        metavar='OAUTH2_CREDENTIALS_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('~/.config'),
+                            'google-oauthlib-tool',
+                            'credentials.json'
+                        ),
+                        help='Path to store and read OAuth2 credentials')
+	args = parser.parse_args()
+    with open(args.credentials, 'r') as f:
+        credentials = google.oauth2.credentials.Credentials(token=None,
+                                                            **json.load(f))
+
+    print("hotword_detection: Starting Assistant")
+    with Assistant(credentials) as assistant:
+    	assistant.start_conversation()
 	# Stop the  detector until google assistant is done
 	# Turn on google assistant
 	
